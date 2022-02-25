cf97856c59e4cb8765a41a537f4ce0e36537e5d3
austinjs <ajsprunger@gmail.com>
2022-02-25 07:11:45 -0700

    finish parts 1 and 2

---
+++ routes/skills.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import * as skillsCtrl from "../controllers/skills.js"
+import * as skillsDb from "../data/skill-db.js"
+const router = Router()
+
+/* GET users listing. */
+router.get('/', skillsCtrl.index)
+router.get('/new', skillsCtrl.new)
+router.get('/:id', skillsCtrl.show)
+router.post('/', skillsCtrl.create)
+router.delete('/:id', skillsCtrl.delete)
+
+export {
+  router
+}
